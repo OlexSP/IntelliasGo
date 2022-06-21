5234136da5271acca75dc9a1139e2fe92af36612
Alex Al <alexsputnik@gmail.com>
2022-06-21 19:49:46 +0300

    final commit

---
+++ HW/1-SliceDuplicateRemove/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	arr := []int{4, 1, 4, -4, 6, 3, 8, 8}
+	var result []int
+	mapCounter := make(map[int]int, len(arr))
+
+	for _, v := range arr {
+		mapCounter[v]++
+		if mapCounter[v] == 1 {
+			result = append(result, v)
+		}
+	}
+
+	fmt.Printf("Duplicates have been removed: %v\n", result)
+
+}
