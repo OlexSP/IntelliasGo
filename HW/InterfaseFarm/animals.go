a4015d7b7ff07e927594d0a2c3c5fb66ce90507b
Alex Al <alexsputnik@gmail.com>
2022-07-05 14:50:41 +0300

    farmSolution final

--- HW/InterfaseFarm/animals.go
+++ HW/InterfaseFarm/animals.go
@@ -49,3 +49,17 @@ type animal interface {
 	foodIntakeGetter
 	fmt.Stringer
 }
+
+func farmConsume(farm []animal) int {
+	var sumFoodWeight, foodIntake int
+	for _, v := range farm {
+		foodIntake = v.getFoodIntake()
+		if foodIntake <= 0 {
+			fmt.Printf("%s has got improper weight.\n", v.String())
+			return 0
+		}
+		sumFoodWeight += foodIntake
+		fmt.Printf("%s, eats %d kg/month.\n", v.String(), foodIntake)
+	}
+	return sumFoodWeight
+}
--- HW/InterfaseFarm/main.go
+++ HW/InterfaseFarm/main.go
@@ -7,7 +7,7 @@ func main() {
 		Cat{name: "Dambass", weight: 4},
 		Cat{name: "Dana", weight: 8},
 		Cow{name: "Murka", weight: 400},
-		Cow{name: "Mashka", weight: 600},
+		Cow{name: "Mashka", weight: -600},
 		Cow{name: "Mira", weight: 450},
 		Dog{name: "Pirat", weight: 15},
 		Dog{name: "Patron", weight: 12},
@@ -17,18 +17,4 @@ func main() {
 
 	fmt.Printf("The farm needs %v kg of food.\n", farmNeeds)
 
-}
-
-func farmConsume(farm []animal) int {
-	var sumFoodWeight, foodIntake int
-	for _, v := range farm {
-		foodIntake = v.getFoodIntake()
-		if foodIntake <= 0 {
-			fmt.Printf("%s has got improper weight.\n", v.String())
-			return 0
-		}
-		sumFoodWeight += foodIntake
-		fmt.Printf("%s, eats %d kg/month.\n", v.String(), foodIntake)
-	}
-	return sumFoodWeight
 }
