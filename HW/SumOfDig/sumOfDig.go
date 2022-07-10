7573b41c7d0fe6801d5ffbb582967a5655e109ba
Alex Al <alexsputnik@gmail.com>
2022-07-10 13:28:52 +0300

    + sumOfDig recursion

--- HW/SumOfDig/sumOfDig.go
+++ HW/SumOfDig/sumOfDig.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println(DigitalRoot(942))
+	fmt.Println(DigitalRootRecursion(9425))
+	fmt.Println(DigitalRoot2(942))
 
 }
 
@@ -21,4 +23,22 @@ L:
 	}
 
 	return sum
+}
+
+func DigitalRootRecursion(n int) int {
+	sum := 0
+
+	for n > 0 {
+		sum += n % 10
+		n /= 10
+	}
+
+	if sum > 9 {
+		return DigitalRootRecursion(sum)
+	}
+
+	return sum
+}
+func DigitalRoot2(n int) int {
+	return (n-1)%9 + 1
 }
